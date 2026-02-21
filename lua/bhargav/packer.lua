6e34b9003436a220b9ccd17503ef8d5ccf80d44e
Bhargav Akula <mail.bhargavakula@gmail.com>
2026-02-21 18:57:59 -0500

    still need to fix issues with telescope

--- lua/bhargav/packer.lua
+++ lua/bhargav/packer.lua
@@ -8,7 +8,7 @@ return require('packer').startup(function(use)
     use 'wbthomason/packer.nvim'
     -- telescope.lua (for finding files)
     use {
-        'nvim-telescope/telescope.nvim', tag = '0.1.8',
+        'nvim-telescope/telescope.nvim', version = '*',
         -- or                            , branch = '0.1.x',
         requires = { {'nvim-lua/plenary.nvim'} }
     }
