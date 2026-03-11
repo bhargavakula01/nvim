611f6e710edc1f4bc31af7df94e459346b36513c
Bhargav Akula <mail.bhargavakula@gmail.com>
2026-03-11 08:12:26 -0400

    minor update

--- lua/bhargav/remap.lua
+++ lua/bhargav/remap.lua
@@ -1,7 +1,7 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>h", "<cmd>nohlsearch<CR>", { desc = "Clear highlight" })
-vim.api.nvim_set_keymap('n', 'b', 'N', { noremap = true, silent = true })
+-- vim.api.nvim_set_keymap('n', 'b', 'N', { noremap = true, silent = true })
 -- Bind '1' to move to end of line ($)
 vim.keymap.set("n", "~", "$", { noremap = true, silent = true })
 -- Bind '2' to move to first non-whitespace character (^)
